271d5c58e03bb1ea967d7d6c393b5f5c6c07d4ee
Aniket Tiwari <aniket.76907@gmail.com>
2021-06-10 21:32:47 +0530

    layout laid for search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:read_cafe/searchBar.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +11,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'ReadCafe',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: //homepage here
+      home: MyHomePage(), //homepage here
     );
   }
 }
 
 //example structure
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List list = [
+    "The Alchemist",
+    "Wings of Fire",
+    "Secret Seven",
+    "Sherlock Holmes",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*drawer: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/1.jpg'), fit: BoxFit.cover),
+        ),
+        width: MediaQuery.of(context).size.width * 1,
+        child: Drawer(),
+      ),*/
+      appBar: AppBar(
+        //search bar in title
+        title: Row(
+          children: [
+            Expanded(flex: 1, child: Text("ReadCafe")),
+            Expanded(
+              flex: 3,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: GFSearchBar(
+                  searchBoxInputDecoration: InputDecoration(
+                    suffixIcon: Icon(Icons.search),
+                    hintText: "Search here ...",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                  ),
+                  searchList: list,
+                  searchQueryBuilder: (query, list) {
+                    return list
+                        .where((item) =>
+                            '$item'.toLowerCase().contains(query.toLowerCase()))
+                        .toList();
+                  },
+                  overlaySearchListItemBuilder: (item) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white)),
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        '$item',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    );
+                  },
+                  onItemSelected: (item) {
+                    setState(() {
+                      print('$item');
+                    });
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+
+        actions: [
+          ElevatedButton.icon(
+              onPressed: () {}, icon: Icon(Icons.home), label: Text('Home')),
+          ElevatedButton.icon(
+              onPressed: () {}, icon: Icon(Icons.book), label: Text('About')),
+          ElevatedButton.icon(
+              onPressed: () {}, icon: Icon(Icons.chat), label: Text('Chat')),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert_outlined),
+              label: Text(""))
+        ],
+      ),
+
+      //body: ,
+    );
+  }
+}
---
+++ lib/searchBar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+// in this page we will make the working of appbar
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
