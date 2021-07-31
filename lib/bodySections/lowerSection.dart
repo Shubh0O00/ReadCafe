d896a902b1575271ac79146ca7c72d1fccf3c3d6
Aniket Tiwari <aniket.76907@gmail.com>
2021-08-01 03:30:56 +0530

    added slight changes

--- lib/bodySections/lowerSection.dart
+++ lib/bodySections/lowerSection.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:read_cafe/api/bookClass.dart';
 import 'package:read_cafe/api/api.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Lower extends StatefulWidget {
   const Lower({Key? key}) : super(key: key);
@@ -33,9 +34,37 @@ class _LowerState extends State<Lower> {
         itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            //image by url
-            decoration: BoxDecoration(color: Colors.amber),
-          );
+              //image by url
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://i.pinimg.com/originals/9e/07/fe/9e07fe5842ccad12c689fbf2dded4480.jpg"),
+                      fit: BoxFit.cover)),
+
+              // adding child rating bar which will be updated by us
+              // it will show the recorded ratings
+
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: RatingBar.builder(
+                  direction: Axis.horizontal,
+                  minRating: 1,
+
+                  // the main thing is to be done here
+                  // update xth book rating from data base in initial rating
+
+                  initialRating: 3,
+                  onRatingUpdate: (double value) {},
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemSize: 24,
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.white,
+                  ),
+                ),
+                // pass the url data in network image
+              ));
         },
       ),
     );
--- lib/bodySections/singleCard.dart
+++ lib/bodySections/singleCard.dart
@@ -10,7 +10,7 @@ class SingleCard extends StatelessWidget {
       // in this container we'll make the upper part showing book selected
       // reviews written and you can rate that particular book
       width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.5,
+      height: MediaQuery.of(context).size.height * 0.75,
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('assets/images/3.jpg'), fit: BoxFit.cover)),
